314880aa4055f5b6830f476e94e417980750d642
Cgameworld <support@cgameworld.com>
2019-06-23 01:11:12 -0700

    updated README

--- RoadDumpTools/RoadDumpTools.cs
+++ RoadDumpTools/RoadDumpTools.cs
@@ -5,6 +5,6 @@ namespace RoadDumpTools
     public class RoadDumpToolsMod : IUserMod
     {
         public string Name => "Road Dump Tools";
-        public string Description => "adds extra tools in road editor!";
+        public string Description => "Adds extra tools in road editor!";
     }
 }
